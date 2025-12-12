c03a51e8598dbad9b2d316978c0c94eef81ae94d
Matias Narganes <97039933+matiasnarganes1@users.noreply.github.com>
2025-12-12 15:07:04 +0000

    chore: update allowed hosts and api_root for Codespaces compatibility

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-wp61lpfx&09wo5f$niqr6i)zco8sr6e1&amsc*69nn_%*q9y5n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
